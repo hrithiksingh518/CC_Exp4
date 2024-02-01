f0273fe9584653d0f168436c9b3970c35f06cd70
hrithiksingh518 <77116538+hrithiksingh518@users.noreply.github.com>
2024-02-01 13:39:03 +0530

    Add files via upload

---
+++ Exp. 1.1.1.py
@@ -0,0 +1,12 @@
+# WAP to enter two number and perform all arithmetic operations.
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+print("Addition: ", x+y)
+print("Subtraction: ", x-y)
+print("Multiplication: ", x*y)
+print("Division: ", x/y)
+print("Modulus: ", x%y)
+print("Exponentiation: ", x**y)
+print("Floor Division: ", x//y)
